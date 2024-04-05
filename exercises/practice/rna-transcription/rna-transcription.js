837a80832c2a9be092594893542c8aa36f4cb0dc
nancy <liyingxiong23@gmail.com>
2024-04-06 02:03:16 +1100

    31. rna-transcription

--- exercises/practice/rna-transcription/rna-transcription.js
+++ exercises/practice/rna-transcription/rna-transcription.js
@@ -3,6 +3,16 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRna = () => {
-  throw new Error('Remove this statement and implement this function');
+export const toRna = (input) => {
+  return input
+    .split('')
+    .map((i) => hashMap[i])
+    .join('');
+};
+
+export const hashMap = {
+  G: 'C',
+  C: 'G',
+  T: 'A',
+  A: 'U',
 };
