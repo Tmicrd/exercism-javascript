8f7f430e49049c2063a0be067852c6a57ba49faf
nancy <liyingxiong23@gmail.com>
2024-04-05 21:21:29 +1100

    8. bird-watcher

--- exercises/concept/bird-watcher/bird-watcher.js
+++ exercises/concept/bird-watcher/bird-watcher.js
@@ -11,7 +11,11 @@
  * @returns {number} total bird count
  */
 export function totalBirdCount(birdsPerDay) {
-  throw new Error('Please implement the totalBirdCount function');
+  let sum = 0;
+  for (let i = 0; i < birdsPerDay.length; i++) {
+    sum += birdsPerDay[i];
+  }
+  return sum;
 }
 
 /**
@@ -21,9 +25,28 @@ export function totalBirdCount(birdsPerDay) {
  * @param {number} week
  * @returns {number} birds counted in the given week
  */
+
 export function birdsInWeek(birdsPerDay, week) {
-  throw new Error('Please implement the birdsInWeek function');
+  const weekIndex = (week - 1) * 7;
+  return totalBirdCount(birdsPerDay.slice(weekIndex, weekIndex + 7));
 }
+
+// Another solution
+// export function birdsInWeek(birdsPerDay, week) {
+//   let weeks = [];
+
+//   for (let i = 0; i < birdsPerDay.length; i += 7) {
+//     let sum = 0;
+
+//     for (let j = i; j < i + 7 && j < birdsPerDay.length; j++) {
+//       sum += birdsPerDay[j];
+//     }
+
+//     weeks.push(sum);
+//   }
+
+//   return weeks[week-1]
+// }
 
 /**
  * Fixes the counting mistake by increasing the bird count
@@ -33,5 +56,10 @@ export function birdsInWeek(birdsPerDay, week) {
  * @returns {number[]} corrected bird count data
  */
 export function fixBirdCountLog(birdsPerDay) {
-  throw new Error('Please implement the fixBirdCountLog function');
+  for (let i = 0; i < birdsPerDay.length; i++) {
+    if (i % 2 === 0) {
+      birdsPerDay[i] = birdsPerDay[i] + 1;
+    }
+  }
+  return birdsPerDay;
 }
