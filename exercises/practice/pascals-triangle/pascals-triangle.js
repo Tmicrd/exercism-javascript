b603bd69b200b7d4b09e209e40928a3517ea2580
nancy <liyingxiong23@gmail.com>
2024-04-06 02:13:34 +1100

    37. pascals-triangle

--- exercises/practice/pascals-triangle/pascals-triangle.js
+++ exercises/practice/pascals-triangle/pascals-triangle.js
@@ -3,6 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-export const rows = () => {
-  throw new Error('Remove this statement and implement this function');
+export const rows = (num) => {
+  if (num === 0) return [];
+
+  const triangle = new Array(num)
+    .fill([])
+    .reduce((cur) => [...cur, createNewRow(cur[cur.length - 1])], []);
+  return triangle;
+};
+
+const createNewRow = (subArr) => {
+  if (!subArr) subArr = [];
+  let newRow = Array.from(
+    { length: subArr.length + 1 },
+    (el, i) => subArr[i - 1] + subArr[i] || 1,
+  );
+
+  return newRow;
 };
