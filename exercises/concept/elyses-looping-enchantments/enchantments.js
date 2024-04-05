4a359570f23e20799e4055ccb755bcfaf54ae9c7
nancy <liyingxiong23@gmail.com>
2024-04-06 00:56:49 +1100

    13. elyses-looping-enchantments

--- exercises/concept/elyses-looping-enchantments/enchantments.js
+++ exercises/concept/elyses-looping-enchantments/enchantments.js
@@ -9,7 +9,11 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  throw new Error('Implement the cardTypeCheck function');
+  let count = 0;
+  stack.forEach((item) => {
+    item === card ? ++count : null;
+  });
+  return count;
 }
 
 /**
@@ -20,5 +24,10 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  throw new Error('Implement the determineOddEvenCards function');
+  let count = 0;
+  stack.forEach((item) => {
+    type && item % 2 === 0 ? count++ : null;
+    !type && item % 2 !== 0 ? count++ : null;
+  });
+  return count;
 }
