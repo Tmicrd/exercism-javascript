11e9caf9df96b9f24f535391625075055f940580
nancy <liyingxiong23@gmail.com>
2024-04-06 01:34:50 +1100

    21. lasagna-master

--- exercises/concept/lasagna-master/lasagna-master.js
+++ exercises/concept/lasagna-master/lasagna-master.js
@@ -10,3 +10,33 @@
  *   ...
  * }
  */
+export const cookingStatus = (timer) => {
+  return timer === 0
+    ? 'Lasagna is done.'
+    : timer > 0
+    ? 'Not done, please wait.'
+    : 'You forgot to set the timer.';
+};
+
+export const preparationTime = (layers, num) => {
+  return layers.length * (num ? num : 2);
+};
+
+export const quantities = (layers) => {
+  return {
+    noodles: layers.filter((noodles) => noodles === 'noodles').length * 50,
+    sauce: layers.filter((sauce) => sauce === 'sauce').length * 0.2,
+  };
+};
+
+export const addSecretIngredient = (array1, array2) => {
+  array2.push(array1[array1.length - 1]);
+};
+
+export const scaleRecipe = (recipe, num) => {
+  let scaled = {};
+  for (const key in recipe) {
+    scaled[key] = (recipe[key] * num) / 2;
+  }
+  return scaled;
+};
