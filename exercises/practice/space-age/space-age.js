7bde21fd974247cb0f4f27832f905c48cd73f64b
nancy <liyingxiong23@gmail.com>
2024-04-06 02:04:48 +1100

    32. space-age

--- exercises/practice/space-age/space-age.js
+++ exercises/practice/space-age/space-age.js
@@ -3,6 +3,18 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this statement and implement this function');
+export const age = (chosenPlanet, seconds) => {
+  return Number((seconds / EARTH_YEAR / PLANET[chosenPlanet]).toFixed(2));
 };
+
+export const PLANET = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+  },
+  EARTH_YEAR = 31557600;
