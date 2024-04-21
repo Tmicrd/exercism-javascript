ee5d6aef0b6b9a4c29e83fd098afced966521d56
nancy <liyingxiong23@gmail.com>
2024-04-21 10:20:57 +1000

    word-count

--- exercises/practice/word-count/word-count.js
+++ exercises/practice/word-count/word-count.js
@@ -3,6 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-export const countWords = () => {
-  throw new Error('Remove this statement and implement this function');
+export const countWords = (str) => {
+  const newStr = str
+    .toLowerCase()
+    .replace(/[^\w\s']+/g, ' ')
+    .match(/\S+/g);
+  const getNewWords = () => {
+    return newStr.map((str) => {
+      return str.replace(/^'|'$/g, '');
+    });
+  };
+
+  const words = getNewWords();
+
+  return words.reduce((pre, cur) => {
+    pre[cur] = pre[cur] ? pre[cur] + 1 : 1;
+    return pre;
+  }, {});
 };
