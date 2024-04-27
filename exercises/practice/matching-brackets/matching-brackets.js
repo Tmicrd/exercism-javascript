75006d5ac320cca4ea68597b007114b7a5d4bba9
nancy <liyingxiong23@gmail.com>
2024-04-27 17:56:09 +1000

    matching-brackets

--- exercises/practice/matching-brackets/matching-brackets.js
+++ exercises/practice/matching-brackets/matching-brackets.js
@@ -3,6 +3,11 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPaired = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isPaired = (string) => {
+  string = string.replace(/[^{}\[\]\(\)]/g, ''); //eslint-disable-line
+  for (let len = -1; len !== string.length; ) {
+    len = string.length;
+    string = string.replace(/\{\}|\[\]|\(\)/g, '');
+  }
+  return string.length === 0;
 };
