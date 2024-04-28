f661b6ae2c4fddaec19860c1b1f0ee4cac94cbb8
nancy <liyingxiong23@gmail.com>
2024-04-28 10:29:26 +1000

    scale-generator

--- exercises/practice/scale-generator/scale-generator.js
+++ exercises/practice/scale-generator/scale-generator.js
@@ -3,16 +3,51 @@
 // convenience to get you started writing code faster.
 //
 
+const SCALE = {
+  sharp: ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'],
+  flat: ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'],
+};
+
+const INTERVAL = {
+  m: 1,
+  M: 2,
+  A: 3,
+};
+
 export class Scale {
-  constructor(tonic) {
-    throw new Error('Remove this statement and implement this function');
+  constructor(tonicMode) {
+    const scale = isFlat(tonicMode) ? SCALE.flat : SCALE.sharp;
+    const tonic = `${tonicMode[0].toUpperCase()}${tonicMode[1] ?? ''}`;
+    const index = scale.indexOf(tonic);
+    this.scale = scale.slice(index).concat(scale.slice(0, index));
   }
-
   chromatic() {
-    throw new Error('Remove this statement and implement this function');
+    return [...this.scale];
   }
-
   interval(intervals) {
-    throw new Error('Remove this statement and implement this function');
+    let pos = 0;
+    let output = [this.scale[pos]];
+    let ints = intervals.split('');
+    for (let i = 0; i < ints.length; i++) {
+      pos = (pos + INTERVAL[ints[i]]) % this.scale.length;
+      output.push(this.scale[pos]);
+    }
+    return output;
   }
+}
+function isFlat(tonic) {
+  return [
+    'F',
+    'Bb',
+    'Eb',
+    'Ab',
+    'Db',
+    'Gb',
+    'd',
+    'g',
+    'c',
+    'f',
+    'bb',
+    'eb',
+  ].includes(tonic);
 }
