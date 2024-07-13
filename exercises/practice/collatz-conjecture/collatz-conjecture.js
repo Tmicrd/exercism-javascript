c9869cd8402702488c86008fcfd77029be0f2068
nancy <liyingxiong23@gmail.com>
2024-07-13 19:35:30 +1000

    collatz-conjecture

--- exercises/practice/collatz-conjecture/collatz-conjecture.js
+++ exercises/practice/collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,18 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error('Remove this statement and implement this function');
+export const steps = (n) => {
+  if (n <= 0) {
+    throw new Error('Only positive numbers are allowed');
+  }
+
+  const iterate = (number, step = 0) => {
+    while (number !== 1) {
+      number = number % 2 === 0 ? number / 2 : 3 * number + 1;
+      step++;
+    }
+    return step;
+  };
+
+  return iterate(n);
 };
