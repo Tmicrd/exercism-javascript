a5f1f8a4b0a16ff3cd90d5b44bd5b8b5dc0b6a7c
nancy <liyingxiong23@gmail.com>
2024-07-13 19:47:41 +1000

    triangle

--- exercises/practice/triangle/triangle.js
+++ exercises/practice/triangle/triangle.js
@@ -5,18 +5,30 @@
 
 export class Triangle {
   constructor(...sides) {
-    throw new Error('Remove this statement and implement this function');
+    this.sides = sides;
+  }
+
+  isValid() {
+    const [s1, s2, s3] = this.sides;
+    return (
+      s1 > 0 &&
+      s2 > 0 &&
+      s3 > 0 &&
+      s1 + s2 >= s3 &&
+      s1 + s3 >= s2 &&
+      s2 + s3 >= s1
+    );
   }
 
   get isEquilateral() {
-    throw new Error('Remove this statement and implement this function');
+    return this.isValid() && this.sides.every((side) => side === this.sides[0]);
   }
 
   get isIsosceles() {
-    throw new Error('Remove this statement and implement this function');
+    return this.isValid() && new Set(this.sides).size <= 2;
   }
 
   get isScalene() {
-    throw new Error('Remove this statement and implement this function');
+    return this.isValid() && new Set(this.sides).size === 3;
   }
 }
