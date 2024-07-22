68048f9ac6a7e784349e3f8de4da758a431ce0e1
nancy <liyingxiong23@gmail.com>
2024-07-22 23:08:56 +1000

    etl

--- exercises/practice/etl/etl.js
+++ exercises/practice/etl/etl.js
@@ -3,6 +3,14 @@
 // convenience to get you started writing code faster.
 //
 
-export const transform = () => {
-  throw new Error('Remove this statement and implement this function');
+export const transform = (input) => {
+  return Object.keys(input).reduce((output, key) => {
+    const items = input[key] || [];
+
+    for (const item of items) {
+      output[item.toLowerCase()] = Number(key);
+    }
+
+    return output;
+  }, {});
 };
