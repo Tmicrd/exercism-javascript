8213ba9f1ff2285549414efc11592229e3280f1e
nancy <liyingxiong23@gmail.com>
2024-07-28 20:49:51 +1000

    pythagorean-triplet

--- exercises/practice/pythagorean-triplet/pythagorean-triplet.js
+++ exercises/practice/pythagorean-triplet/pythagorean-triplet.js
@@ -1,18 +1,50 @@
-//
-// This is only a SKELETON file for the 'Pythagorean Triplet' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export function triplets({ minFactor, maxFactor, sum }) {
-  throw new Error('Remove this statement and implement this function');
-}
-
 class Triplet {
   constructor(a, b, c) {
-    throw new Error('Remove this statement and implement this function');
+    this.a = a;
+    this.b = b;
+    this.c = c;
   }
 
   toArray() {
-    throw new Error('Remove this statement and implement this function');
+    return [this.a, this.b, this.c];
   }
+
+  get isPythagorean() {
+    return this.a ** 2 + this.b ** 2 === this.c ** 2;
+  }
+
+  get sum() {
+    return this.a + this.b + this.c;
+  }
+}
+
+export function triplets({ minFactor = 1, maxFactor, sum }) {
+  const min = minFactor;
+  const max = maxFactor || sum - 1;
+
+  const isDesiredTriplet = (triplet) => {
+    return triplet.isPythagorean && (!sum || triplet.sum === sum);
+  };
+
+  const result = [];
+  const squaredMap = new Map();
+
+  for (let a = min; a < max; a++) {
+    squaredMap.set(a ** 2, a);
+  }
+
+  for (let a = min; a < max - 1; a++) {
+    for (let b = a + 1; b < max; b++) {
+      const cSquared = a ** 2 + b ** 2;
+      if (squaredMap.has(cSquared)) {
+        const c = squaredMap.get(cSquared);
+        const triplet = new Triplet(a, b, c);
+        if (isDesiredTriplet(triplet)) {
+          result.push(triplet);
+        }
+      }
+    }
+  }
+
+  return result;
 }
