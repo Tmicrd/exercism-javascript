a088b4f522a90dfb4be14e9e518302d443a38ab4
nancy <liyingxiong23@gmail.com>
2024-07-28 20:43:45 +1000

    grains

--- exercises/practice/grains/grains.js
+++ exercises/practice/grains/grains.js
@@ -9,10 +9,32 @@
 // This is only a SKELETON file for the 'Grains' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-export const square = () => {
-  throw new Error('Remove this statement and implement this function');
+
+/**
+ * Gets the number of grains on the nth square
+ *
+ * @param {Number} num Number to compute its square
+ *
+ * @return {BigInt} Square of num
+ */
+export const square = (num) => {
+  if (num < 1 || num > 64) {
+    throw new Error('square must be between 1 and 64');
+  }
+  return BigInt(2) ** BigInt(num - 1);
 };
 
+/**
+ * Gets the total number of grains on all squares
+ *
+ * @return {BigInt} Sum of all squares
+ */
 export const total = () => {
-  throw new Error('Remove this statement and implement this function');
+  let result = BigInt(0);
+
+  for (let squareNum = 1; squareNum <= 64; squareNum++) {
+    result += square(squareNum);
+  }
+
+  return result;
 };
