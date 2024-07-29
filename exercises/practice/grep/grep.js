e19f307ede3555ef43b225bb9ecfa94fdf60fc92
nancy <liyingxiong23@gmail.com>
2024-07-29 23:21:41 +1000

    grep

--- exercises/practice/grep/grep.js
+++ exercises/practice/grep/grep.js
@@ -1,22 +1,33 @@
-#!/usr/bin/env node
-
-// The above line is a shebang. On Unix-like operating systems, or environments,
-// this will allow the script to be run by node, and thus turn this JavaScript
-// file into an executable. In other words, to execute this file, you may run
-// the following from your terminal:
-//
-// ./grep.js args
-//
-// If you don't have a Unix-like operating system or environment, for example
-// Windows without WSL, you can use the following inside a window terminal,
-// such as cmd.exe:
-//
-// node grep.js args
-//
-// Read more about shebangs here: https://en.wikipedia.org/wiki/Shebang_(Unix)
-
 const fs = require('fs');
 const path = require('path');
+
+const VALID_OPTIONS = [
+  'n', // add line numbers
+  'l', // print file names where pattern is found
+  'i', // ignore case
+  'v', // reverse files results
+  'x', // match entire line
+];
+
+const ARGS = process.argv;
+
+function checkLineMatchesPattern(line, pattern) {
+  let left = line;
+  let right = pattern;
+
+  // ignore case
+  if (isOptionSet('i')) {
+    left = line.toLowerCase();
+    right = pattern.toLowerCase();
+  }
+
+  // match entire line
+  if (isOptionSet('x')) {
+    return left === right;
+  }
+
+  return left.match(right) !== null;
+}
 
 /**
  * Reads the given file and returns lines.
@@ -31,19 +42,83 @@ function readLines(file) {
   return data.split(/\r?\n/);
 }
 
-const VALID_OPTIONS = [
-  'n', // add line numbers
-  'l', // print file names where pattern is found
-  'i', // ignore case
-  'v', // reverse files results
-  'x', // match entire line
-];
+function getConfigFromArgs() {
+  const config = {
+    pattern: '',
+    options: [],
+    files: [],
+  };
 
-const ARGS = process.argv;
+  let patternFound = false;
 
-//
-// This is only a SKELETON file for the 'Grep' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-// This file should *not* export a function. Use ARGS to determine what to grep
-// and use console.log(output) to write to the standard output.
+  ARGS.slice(2).forEach((val) => {
+    if (patternFound) {
+      config.files.push(val);
+      return;
+    }
+
+    if (val.indexOf('-') !== -1) {
+      const option = val.replace('-', '');
+
+      if (!VALID_OPTIONS.includes(option)) {
+        throw new Error(`Unknown option ${option}`);
+      }
+
+      config.options.push(option);
+      return;
+    }
+
+    patternFound = true;
+    config.pattern = val;
+  });
+
+  return config;
+}
+
+const { options, pattern, files } = getConfigFromArgs();
+const isOptionSet = (option) => options.includes(option);
+
+files.forEach((file) => {
+  const lines = readLines(file);
+
+  // print file names where pattern is found
+  if (isOptionSet('l')) {
+    const foundMatch = lines.find((line) => {
+      const lineMatchesPattern = checkLineMatchesPattern(line, pattern);
+
+      // reverse files results
+      return isOptionSet('v') ? !lineMatchesPattern : lineMatchesPattern;
+    });
+
+    if (foundMatch) {
+      console.log(file);
+    }
+
+    return;
+  }
+
+  lines.forEach((line, index) => {
+    let result = '';
+    let shouldOutputLine = checkLineMatchesPattern(line, pattern);
+
+    // reverse files results
+    if (isOptionSet('v')) {
+      shouldOutputLine = !shouldOutputLine;
+    }
+
+    if (shouldOutputLine) {
+      if (files.length > 1) {
+        result += `${file}:`;
+      }
+
+      // add line numbers
+      if (isOptionSet('n')) {
+        result += `${index + 1}:`;
+      }
+
+      result += line;
+
+      console.log(result);
+    }
+  });
+});
