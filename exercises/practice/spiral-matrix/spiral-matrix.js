0ba90d41a0f694e41ccfd7245bfa04093857c591
nancy <liyingxiong23@gmail.com>
2024-08-06 22:34:39 +1000

    spiral-matrix

--- exercises/practice/spiral-matrix/spiral-matrix.js
+++ exercises/practice/spiral-matrix/spiral-matrix.js
@@ -3,6 +3,33 @@
 // convenience to get you started writing code faster.
 //
 
-export const spiralMatrix = () => {
-  throw new Error('Remove this statement and implement this function');
+export const spiralMatrix = (size) => {
+  const spiral = Array.from({ length: size }, () => Array(size).fill(0));
+  const totalNumbers = size ** 2;
+  let currentNumber = 1;
+  let topLeft = 0;
+  let bottomRight = size - 1;
+
+  while (currentNumber <= totalNumbers) {
+    for (let x = topLeft; x <= bottomRight; x++) {
+      spiral[topLeft][x] = currentNumber++;
+    }
+
+    for (let y = topLeft + 1; y <= bottomRight; y++) {
+      spiral[y][bottomRight] = currentNumber++;
+    }
+
+    for (let x = bottomRight - 1; x >= topLeft; x--) {
+      spiral[bottomRight][x] = currentNumber++;
+    }
+
+    for (let y = bottomRight - 1; y > topLeft; y--) {
+      spiral[y][topLeft] = currentNumber++;
+    }
+
+    topLeft++;
+    bottomRight--;
+  }
+
+  return spiral;
 };
